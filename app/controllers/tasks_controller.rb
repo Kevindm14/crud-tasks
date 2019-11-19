d8cd79e737b7ec2f16154c68bc417d68a1519749
Kevindm14 <kevindiazm.14@gmail.com>
2019-11-18 23:28:49 -0500

    refactor styles

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!
   before_action :set_task, only: [:edit, :update, :destroy]
   
   def index
